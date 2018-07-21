7d650f824fa9acf56f2d016f0f1d066943bf7635
Tetsuro Takemoto <t@ineeza.com>
2018-07-21 16:36:32 +0900

    Update Home style

--- src/layouts/home/Home.js
+++ src/layouts/home/Home.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+// UI Components
+import LoginButtonContainer from '../../user/ui/loginbutton/LoginButtonContainer'
+
 class Home extends Component {
   render() {
     return(
@@ -6,9 +9,9 @@ class Home extends Component {
         <div className="home-content">
           <div className="pure-u-1-1">
             <h1>Welcome to Quick Pass!</h1>
-            <img src="https://www.w3schools.com/html/pic_trulli.jpg" alt="Trulli" width="500" height="333"></img>
           </div>
         </div>
+        <img className="fit" src="https://i.pinimg.com/originals/e8/4f/35/e84f3550abe53da48f47f6b1dbada5f6.jpg" alt="Trulli" ></img>
       </main>
     )
   }
