d8f0a8eefd6fef13c7d907a874c3956687363d68
Tetsuro Takemoto <t@ineeza.com>
2018-07-22 01:47:18 +0900

    Add contract addresses of quick-pass tokens

--- src/util/settings.js
+++ src/util/settings.js
@@ -1,5 +1,11 @@
 export default {
   // ethAddressDefault: "0x33A464CdA2CA86710b6d37F20DB92192E4595DCE",
   // erc20Address: "0xdf4fae0cf4109ad7afa39bbddfaa4dc5637dc5ca"
-  erc20Address: "0xabd3bdea246a6d9fd0f3a488028e35f0d580951d" // QPT address
+  erc20Address: "0xabd3bdea246a6d9fd0f3a488028e35f0d580951d", // QPT address
+  qptContractAddress_1: "0xabd3bdea246a6d9fd0f3a488028e35f0d580951d",
+  qptContractAddress_2: "0x933395d1211efba583a874f43edfb31548d9f08e",
+  qptContractAddress_3: "0x193c7f312f05be30a46fd60c93a073c198a30184",
+  qptContractAddress_4: "0x3f972a1d3505a9f59fdc5c50a75ba9841356e606",
+  qptContractAddress_5: "0xe3433bd8d15d1e0fbf59496a2120a98daaeba18b",
+  qptContractAddress_6: "0xb521ba880d70d842d8ba2e914b7cdc613f442ec0"
 }
