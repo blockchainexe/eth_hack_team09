08671af65b971c95c1a2bdf823fad75260edc618
ys1111 <y0suke111152@gmail.com>
2018-07-22 07:04:31 +0900

    fix conflict

--- src/layouts/markets/Market.js
+++ src/layouts/markets/Market.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
 import QPTcards from '../../components/QPTcards';
 
 class Market extends Component {
   render() {
-    
     return(
       <main className="container" >
         <QPTcards/>
