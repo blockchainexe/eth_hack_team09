1403662e26550dd0ad87e71b7e1ab5c76fda84bc
Tetsuro Takemoto <t@ineeza.com>
2018-07-21 15:55:53 +0900

    Update Home

--- src/layouts/home/Home.js
+++ src/layouts/home/Home.js
@@ -3,9 +3,10 @@ class Home extends Component {
   render() {
     return(
       <main className="container">
-        <div className="pure-g">
+        <div className="home-content">
           <div className="pure-u-1-1">
             <h1>Welcome to Quick Pass!</h1>
+            <img src="https://www.w3schools.com/html/pic_trulli.jpg" alt="Trulli" width="500" height="333"></img>
           </div>
         </div>
       </main>
