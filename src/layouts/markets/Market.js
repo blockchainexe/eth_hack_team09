9b6bbcf54804f8a997acad75a265b17e7fb2f8af
ys1111 <y0suke111152@gmail.com>
2018-07-22 09:08:55 +0900

    fix send token in dashboar

--- src/layouts/markets/Market.js
+++ src/layouts/markets/Market.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import QPTcards from '../../components/QPTcards';
+import { qptContract } from '../dashboard/actions'
 
 class Market extends Component {
   render() {
+    
+    console.log(qptContract)
+
     return(
       <main className="container" >
         <QPTcards/>
